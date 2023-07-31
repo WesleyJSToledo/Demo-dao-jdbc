f07ec7049812bbd5a8feeb06411400d59fb85515
WesleyJSToledo <wesleyjstoledo@gmail.com>
2023-07-31 10:46:48 -0300

    find by id implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,34 +1,84 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.exception.DBException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
+
+	private static String SQL_FIND_BY_ID = "SELECT s.*, d.Name as DepName FROM seller s "
+			+ "INNER JOIN department d ON s.DepartmentId = d.Id WHERE s.Id = ?";
+
+	private Connection connection;
+
+	public SellerDaoJDBC(Connection connection) {
+		this.connection = connection;
+	}
 
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+			pst = connection.prepareStatement(SQL_FIND_BY_ID);
+			pst.setInt(1, id);
+
+			rs = pst.executeQuery();
+
+			if (rs.next()) {
+				Department dep = new Department();
+				Seller sel = new Seller();
+
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+
+				sel.setId(rs.getInt("Id"));
+				sel.setName(rs.getString("Name"));
+				sel.setEmail(rs.getString("Email"));
+				sel.setBirthDate(rs.getDate("BirthDate"));
+				sel.setBaseSalary(rs.getDouble("BaseSalary"));
+				sel.setDepartment(dep);
+
+				return sel;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			try {
+				pst.close();
+				rs.close();
+			} catch (SQLException e) {
+				throw new DBException(e.getMessage());
+			}
+
+		}
 	}
 
 	@Override
